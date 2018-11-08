db2fabce3033255b457013f980d57a224882e14a
Thomas Anderson <thomas@tjanderson.net>
2018-11-08 09:32:29 -0800

    Added sdkman and using sublime again for some editing

--- zsh/extensions/keybinding.zsh
+++ zsh/extensions/keybinding.zsh
@@ -4,3 +4,4 @@ bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+bindkey '^R' history-incremental-search-backward
