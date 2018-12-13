e4af1c3cdea8d47254387c010f90691c9e8c3255
Thomas Anderson <thomas@tjanderson.net>
2018-12-13 08:29:09 -0800

    Minor changes to my config

--- zsh/extensions/gpg.zsh
+++ zsh/extensions/gpg.zsh
@@ -3,8 +3,9 @@ alias gpgquitagent='gpg-connect-agent killagent /bye'
 alias gpgstatus='gpg --card-status'
 
 # Allow gpg controlled SSH keys
-if [[ -x /usr/local/bin/gpgconf ]]; then
-  gpgconf --launch gpg-agent
-  export GPG_TTY=$(tty)
-  export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
-fi
+# Enable when we want to use a yubikey as our hardware based ssh key
+#if [[ -x /usr/local/bin/gpgconf ]]; then
+#  gpgconf --launch gpg-agent
+#  export GPG_TTY=$(tty)
+#  export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+#fi
